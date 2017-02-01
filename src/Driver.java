54a7c6808f0a1ae6043ebb9f51ed94a5e4e7b2c6
trippsupp <trippsupp@gmail.com>
2017-02-01 16:48:47 -0500

    Added invokLater method for main method.

--- src/Driver.java
+++ src/Driver.java
@@ -1,17 +1,25 @@
+
+import javax.swing.SwingUtilities;
+
 /**
  * Contains the main method
- * 
+ *
  * @author Vance Field
  * @version 5-Apr-2016
  */
 public class Driver {
-    
+
     /**
      * Starts the program
-     * @param args 
+     *
+     * @param args
      */
-    public static void main(String[] args){
-        GUI gui = new GUI();
-        gui.display();
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                GUI gui = new GUI();
+                gui.display();
+            }
+        });
     }
 }
